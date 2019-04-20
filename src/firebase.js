3f42646305ec083c98e69d06fdd5d9776b8eea81
mariebawanan <bawananmarieflor@gmail.com>
2019-04-20 14:58:09 +0800

    Configure firebase

---
+++ src/firebase.js
@@ -0,0 +1,29 @@
+import * as firebase from 'firebase';
+import * as FIREBASE_CONFIG from './firebaseConfig';
+
+const config = {
+	apiKey: FIREBASE_CONFIG.API_KEY,
+	authDomain: FIREBASE_CONFIG.AUTH_DOMAIN,
+	databaseURL: FIREBASE_CONFIG.DATABASE_URL,
+	projectId: FIREBASE_CONFIG.PROJECT_ID,
+	storageBucket: FIREBASE_CONFIG.STORAGE_BUCKET,
+	messagingSenderId: FIREBASE_CONFIG.MESSAGING_SENDER_ID
+};
+
+firebase.initializeApp(config);
+
+const firebaseDB = firebase.database();
+const firebaseLogs = firebaseDB.ref('logs');
+
+const firebaseLooper = snapshot => {
+	const data = [];
+	snapshot.forEach(childSnapshot => {
+		data.push({
+			...childSnapshot.val(),
+			id: childSnapshot.key
+		});
+	});
+	return data;
+};
+
+export { firebase, firebaseDB, firebaseLogs, firebaseLooper };
