611f21cfd0ef10fe3f48edaa9e01d5b91dc9c703
mariebawanan <bawananmarieflor@gmail.com>
2019-04-20 13:06:54 +0800

    Decorated LogInput elements

--- src/components/LogInput/LogInput.js
+++ src/components/LogInput/LogInput.js
@@ -2,30 +2,66 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 
 const LogInputContainer = styled('div')`
-	grid-area: 'LogInput';
-	grid-column: 1/2;
-	border: 1px solid var(--primary);
+	display: grid;
+	justify-self: center;
+	grid-gap: 1em;
 `;
 
-const Title = styled('h1')`
+const Title = styled('span')`
+	font-size: 60px;
 	color: var(--primary);
 `;
 
-const LogInputForm = styled('input')`
-	color: var(--black);
+const Cursive = styled(Title)`
+	font-family: 'Meddon', cursive;
+`;
+
+const LogInputForm = styled('textArea')`
+	font-size: 14px;
+	padding: 10px;
+	box-sizing: border-box;
+	border: 1px solid #000000;
+	resize: none;
+	margin-top: 5%;
+	font-family: 'Quicksand', sans-serif;
+	background-color: #ffffff;
+	&:focus {
+		outline: none !important;
+		border: 1px dashed #6acccb;
+	}
 `;
 
 const Submit = styled('button')`
-	color: var(--primary);
+	color: #ffffff;
+	font-size: 1.25rem;
+	background: #6acccb;
+	padding: 5px 20px;
+	border: 1px solid #fff;
+	cursor: pointer;
+	&:focus {
+		outline: 0;
+	}
+	&:hover {
+		color: #6acccb;
+		background: #fff;
+		border: 1px solid #6acccb;
+		font-weight: 700;
+	}
 `;
 
 class LogInput extends Component {
 	render() {
 		return (
 			<LogInputContainer>
-				<Title>logit!</Title>
-				<LogInputForm placeholder='Hello, start writing...' />
-				<Submit> write log. </Submit>
+				<Title>
+					log<Cursive>it!</Cursive>
+				</Title>
+				<LogInputForm
+					rows='15'
+					cols='50'
+					placeholder='Hello, start writing...'
+				/>
+				<Submit disabled> write log. </Submit>
 			</LogInputContainer>
 		);
 	}
