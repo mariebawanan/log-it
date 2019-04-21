5b2402c5fdfe5f7c863f4593e5429eaf90d0f765
mariebawanan <bawananmarieflor@gmail.com>
2019-04-21 12:50:03 +0800

    Clean up

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
-export const API_KEY = 
-export const AUTH_DOMAIN = 
-export const DATABASE_URL = 
-export const PROJECT_ID =
-export const STORAGE_BUCKET =
-export const MESSAGING_SENDER_ID = 
+export const API_KEY = '';
+export const AUTH_DOMAIN = '';
+export const DATABASE_URL = '';
+export const PROJECT_ID = '';
+export const STORAGE_BUCKET = '';
+export const MESSAGING_SENDER_ID = '';
